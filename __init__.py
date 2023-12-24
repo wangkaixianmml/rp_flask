b40b2ce64955c3a992828c26765d076a75d9afe5
Victor Wang <wangkaixian1@163.com>
2023-12-24 20:58:45 +0800

    Base Flask Project

---
+++ __init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+from board import (
+    pages,
+    posts, 
+    database,
+    errors
+)
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_prefixed_env()
+    app.logger.setLevel("INFO")
+
+    database.init_app(app)
+
+    app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+    app.register_error_handler(404, errors.page_not_found)
+
+
+    app.logger.debug(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+    app.logger.debug(f"Using Database: {app.config.get('DATABASE')}")
+
+    return app
